870690016424a387b6b632c4497ee24900a8613a
türkay ürkmez <turkay.urkmez@outlook.com>
2024-09-09 13:56:13 +0300

    initialize repo

---
+++ eshop/src/Services/Catalog/Catalog.Domain/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain
+{
+    public class Product : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+
+        public string? ImageUrl { get; set; }
+        public int? Stock { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+        // TODO 1: Entity yapısını tanımla!
+
+    }
+}
