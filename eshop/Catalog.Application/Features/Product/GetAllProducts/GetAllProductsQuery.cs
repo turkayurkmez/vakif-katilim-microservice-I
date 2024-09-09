51d499c78b4132875be33d341abca0e7186d3d59
türkay ürkmez <turkay.urkmez@outlook.com>
2024-09-09 14:47:25 +0300

    CQRS and mediatr implementation

---
+++ eshop/Catalog.Application/Features/Product/GetAllProducts/GetAllProductsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Features.Product.GetAllProducts
+{
+    public record GetAllProductResponse(int Id, string Name, string? Description, decimal? Price, string? ImageUrl);
+    public class GetAllProductsQuery : IRequest<IEnumerable<GetAllProductResponse>>
+    {
+
+    }
+}
